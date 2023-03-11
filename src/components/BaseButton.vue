dfd26686d8e3993792693661675940d86c19dbac
iduk <iduk.design@gmail.com>
2023-03-11 19:51:56 +0900

    링크버튼, 모달 삭제

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+	<button
+		v-bind="{ ...$attrs }"
+		v-on="$listeners"
+		class="btn"
+		:class="[
+			`btn-${color}`,
+			size ? `btn-${size}` : '',
+			{ block: block },
+		]"
+	>
+		{{ label }}
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	inheritAttrs: true,
+	props: {
+		label: {
+			type: String,
+		},
+		color: {
+			type: String,
+			default: "outline-gray",
+		},
+		size: {
+			type: String,
+			default: "",
+		},
+		block: {
+			type: Boolean,
+			default: false,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
